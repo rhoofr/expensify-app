bd5eb4af131d8b38ee34164eca30002089f5e3a5
Richard Hoofring <rhoofr@yahoo.com>
2020-07-30 13:18:09 -0400

    Added .ds_store to .gitignore and took out hard coded data from app.js

--- src/app.js
+++ src/app.js
@@ -7,28 +7,8 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 
 const store = configureStore();
-
-store.dispatch(
-  addExpense({
-    description: 'Water Bill',
-    amount: 6500,
-    createdAt: 1595900000000,
-  })
-);
-store.dispatch(
-  addExpense({
-    description: 'Gas Bill',
-    amount: 4444,
-    createdAt: 1595952516790,
-  })
-);
-store.dispatch(
-  addExpense({ description: 'Rent', amount: 109500, createdAt: 1595952516777 })
-);
-// store.dispatch(setTextFilter('bill'));
 
 ReactDOM.render(
   <Provider store={store}>
